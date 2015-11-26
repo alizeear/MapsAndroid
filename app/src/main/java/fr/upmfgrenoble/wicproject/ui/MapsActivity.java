7b93b13ece4a7ec50aa9fb0e16a39c9303969f2f
unknown <arn.alizee@gmail.com>
2015-11-26 10:49:45 +0100

    suppression fichier

--- app/src/main/java/fr/upmfgrenoble/wicproject/ui/MapsActivity.java
+++ app/src/main/java/fr/upmfgrenoble/wicproject/ui/MapsActivity.java
@@ -30,7 +30,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mapss);
+        setContentView(R.layout.activity_maps);
         gpx = new GPX();
 
         try {
